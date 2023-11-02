57d6e70ce44d8c65646297156512791a629daff3
Zachary <78396278+EyeIllusions@users.noreply.github.com>
2023-11-02 19:20:39 -0400

    added files that were missing

---
+++ Scrpts/Helper/Iinteractable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Pixelogic.helper
+{
+    public interface Iinteractable
+    {
+        public void Interact();
+    }
+}
